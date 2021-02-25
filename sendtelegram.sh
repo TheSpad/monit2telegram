c6b8f6453feb3cb6b0bda341221b6fbe6f114b72
Adam <git@spad.co.uk>
2021-02-25 19:56:03 +0000

    Remove config file dependency, switch to env

--- sendtelegram.sh
+++ sendtelegram.sh
@@ -21,32 +21,23 @@ while getopts ":c:t:i:p:m:v" opt; do
     esac
 done
 
-# Test config file
-if [ -n "$CONFIGFILE" -a ! -f "$CONFIGFILE" ]; then echo "Configfile not found: $CONFIGFILE"; usage; fi
-
-# Check config file if given
-if [ -n "$CONFIGFILE" ]; then . "$CONFIGFILE";
-# Default config file ~/.telegramrc if it exists
-elif [ -f /etc/telegramrc ]; then . /etc/telegramrc;
-fi
-
-# If TOKEN or CHATID were given in the commandline, then override that in the configfile
-if [ -n "$TOKEN_ARG" ]; then TOKEN=$TOKEN_ARG; fi
-if [ -n "$CHATID_ARG" ]; then CHATID=$CHATID_ARG; fi
+# If TGTOKEN or TGCHATID were given in the commandline, then override that in the configfile
+if [ -n "$TOKEN_ARG" ]; then TGTOKEN=$TOKEN_ARG; fi
+if [ -n "$CHATID_ARG" ]; then TGCHATID=$CHATID_ARG; fi
 
 # Verify parameters
-if [ -z "$TOKEN" ]; then usage "Bot token not set, it must be provided in the config file, or on the command line."; fi;
-if [ -z "$CHATID" ]; then usage "Chat ID not set, it must be provided in the config file, or on the command line."; fi;
+if [ -z "$TGTOKEN" ]; then usage "Bot token not set, it must be provided in the config file, or on the command line."; fi;
+if [ -z "$TGCHATID" ]; then usage "Chat ID not set, it must be provided in the config file, or on the command line."; fi;
 if [ -z "$TEXT" ]; then usage "Message not set, it must be provided on the command line."; fi;
 if [ ! -z $PARSEMODE_ARG ] && [[ "$PARSEMODE_ARG" != +(markdown|html) ]]; then usage "Parse mode must be either 'markdown' or 'html'."; fi;
 
 # Sending to Telegram
-URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+URL="https://api.telegram.org/bot$TGTOKEN/sendMessage"
 TIMEOUT=10
 
-echo "Sending message '$TEXT' to $CHATID"
+echo "Sending message '$TEXT' to $TGCHATID"
 
-CMDARGS="chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT"
+CMDARGS="chat_id=$TGCHATID&disable_web_page_preview=1&text=$TEXT"
 
 if [ ! -z $PARSEMODE_ARG ]; then
     CMDARGS=${CMDARGS}"&parse_mode=$PARSEMODE_ARG"
